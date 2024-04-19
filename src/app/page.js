4d9668ad98ed6c91ecce33308613f9137ebcc7e9
oliullah05 <mdolihasan@gmail.com>
2024-04-19 20:05:56 +0600

    39-4 Create My First Next.Js App. Explore Its File And Folder Structure.

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import React from 'react';
 const HomePage = () => {
   return (
     <div>
-     <h1>Welcome to next js</h1>
+     <h1 className='text-4xl text-center'>Welcome to next js</h1>
     </div>
   );
 };
