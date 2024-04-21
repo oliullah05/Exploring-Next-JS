100c2dfad345253fcac0c47722081401fe03d936
oliullah05 <mdolihasan@gmail.com>
2024-04-21 21:57:07 +0600

    Use Metadata For Better SEO And Import Alias

---
+++ src/app/about/page.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+export const metadata = {
+title:"About"
+}
+
+const About = () => {
+    return (
+        <div>
+          <h1>This is about page</h1> 
+        </div>
+    );
+};
+
+export default About;
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
-
+export const metadata = {
+  title:"Home Page",
+  description:"This is homepage"
+}
 
 const HomePage = () => {
   
   return (
     <div>
      <h1 className='text-4xl  text-center'>Welcome to next js</h1>
-
     </div>
   );
 };
